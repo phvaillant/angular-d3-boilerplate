5860a5aa4110d392f729900f510009732659df51
phvaillant <vaillant@uw.edu>
2017-06-13 14:40:37 -0700

    added angular form features

--- src/landing/landing-ctrl.js
+++ src/landing/landing-ctrl.js
@@ -8,11 +8,29 @@ angular.module('%module%.landing')
 
   $http.get('data/users.csv')
   .then(function(res) {
-    $scope.users = Papa.parse(res.data, {
+    var init_data = Papa.parse(res.data, {
       header: true,
       delimiter: ',',
       dynamicTyping: true,
       skipEmptyLines: true
     }).data;
+    //loop through users
+    init_data.forEach(function(item) {
+      //get initial of each user
+      var inls = item.firstname.replace('-', ' ').match(/\b\w/g).join('') + item.lastname[0];
+      item.initials = inls.toUpperCase();
+      //get roles of each user
+      var roles = item.roles.split(';');
+      item.roles=[];
+      $scope.availableRoles.forEach(function(role) {
+        if (roles.indexOf(role)>-1) {
+          item.roles.push({name:role, selected:true})
+        }
+        else {
+          item.roles.push({name:role, selected:false})
+        }
+      })
+    });
+    $scope.users = init_data;
   });
 });
